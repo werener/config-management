39e4e99acec08fa9101f11ce8f74588106d84567
werener <lenyazhuk@yandex.ru>
2025-09-03 10:54:26 +0300

    Stage 1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import os
 import socket
+import re
 
 LOGIN = os.getlogin()
 HOSTNAME = socket.gethostname()
@@ -12,29 +13,46 @@ WINDOW_HEIGHT = 256
 TERMINAL_HIDDEN = True
 
 root = tk.Tk()
-root.configure(background='blue')
+root.configure(background='black', padx=3, pady=4)
 root.title(f'{LOGIN}@{HOSTNAME}')
 root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
 
-container = tk.Frame(borderwidth=0, pady=0,background='red')
-terminal = ttk.Label(text='',
-                     background='black',
-                     borderwidth=0,
-                     width=WINDOW_WIDTH,
-                     foreground='white')
+container = tk.Frame(borderwidth=0, pady=0, background='red')
+terminal_text = ''
+terminal = tk.Label(text='',
+                    pady=0,
+                    padx=0,
+                    anchor='nw',
+                    background='black',
+                    justify='left',
+                    borderwidth=0,
+                    width=WINDOW_WIDTH,
+                    foreground='white')
 
-input_field = tk.Entry(container,
-                       background='black',
-                       foreground='white',
-                       borderwidth=0,
-                       width=int(WINDOW_WIDTH * 0.75))
+input_field = tk.Entry(
+    container,
+    #    cursor='green',
+    justify='left',
+    background='black',
+    foreground='white',
+    borderwidth=0,
+    selectbackground='white',
+    selectforeground='black',
+    insertbackground='white',
+    insertborderwidth=2,
+    insertwidth=3,
+    insertofftime=0,
+    width=int(WINDOW_WIDTH * 0.75))
 prefix = tk.Label(container,
-                  width=14,
-                  padx=2,
+                  borderwidth=0,
+                  width=15,
+                  padx=0,
+                  anchor='nw',
                   background='black',
-                  foreground='white')
+                  justify='left',
+                  foreground='lime')
 
-prefix['text'] = f'{LOGIN}@{HOSTNAME}$ '
+prefix['text'] = f'{LOGIN}@{HOSTNAME}$'
 
 
 def handleExecuteButton(event=None):
@@ -42,40 +60,80 @@ def handleExecuteButton(event=None):
     def add_to_terminal():
         inp = input_field.get()
         if terminal['text'] == '':
-            terminal['text'] += f'{LOGIN}@{HOSTNAME}$ {inp}'
+            terminal['text'] += f'{LOGIN}@{HOSTNAME}$  {inp}'
         else:
-            terminal['text'] += f'\n{LOGIN}@{HOSTNAME}$ {inp}'
+            terminal['text'] += f'\n{LOGIN}@{HOSTNAME}$  {inp}'
 
     def handle_args():
-        inp = input_field.get().split()
-        args = inp[1:]
-        command = inp[0]
-        match command:
-            case 'exit':
-                root.destroy()
-            case 'ls':
-                terminal['text'] += '\nls ' + ' '.join(args)
+        # try:
+            inp = input_field.get().split()
+            
+            command = inp[0]
+
+            def map_input(inp):
+                parameters, args = [], []
+                for arg in inp:
+                    if re.fullmatch(r"[\'\"].{0,}[\'\"]", arg):
+                        parameters.append(arg)
+                    else:
+                        args.append(arg)
+                return parameters, args
+            
+            parameters, args = map_input(inp[1:])
+
+            match command:
+                case 'exit':
+                    root.destroy()
+                case 'ls':
+                    terminal['text'] += '\nls  args: ' + ' '.join(args) + '  parameters: ' + ' '.join(parameters)
+                case 'cd':
+                    terminal['text'] += '\ncd  args:' + ' '.join(args) + '  parameters' + ' '.join(parameters)
+                case _:
+                    terminal['text'] += f'\ncommand not found: {command}'
+
+        # except:
+        #     print('no input given')
 
     add_to_terminal()
     handle_args()
     global TERMINAL_HIDDEN
     if TERMINAL_HIDDEN:
         TERMINAL_HIDDEN = False
-        terminal.pack(anchor=tk.NW)
+        container.pack_forget()
+
+        terminal.pack(side='top', )
+
+        container.pack(side='top', )
+
+        prefix.pack(in_=container, side="left")
+        input_field.pack(
+            in_=container,
+            side='left',
+        )
+
+        input_field.focus()
     input_field.delete(0, 'end')
+    input_field.focus()
 
 
 root.bind('<Return>', handleExecuteButton)
 
-execute_button = ttk.Button(command=handleExecuteButton,
-                            text='Execute command').pack(side='top',anchor=tk.NE)
+execute_button = tk.Button(command=handleExecuteButton,
+                           borderwidth=0,
+                           background="#1C1C1C",
+                           foreground="#616365",
+                           text='Execute')
+execute_button.pack(side='top', anchor=tk.NE)
 
-terminal.pack(side='top', padx=0)
+# terminal.pack(side='top', )
 
-container.pack(side='top')
+container.pack(side='top', )
 
 prefix.pack(in_=container, side="left")
-input_field.pack(in_=container, side='left')
+input_field.pack(
+    in_=container,
+    side='left',
+)
 
 input_field.focus()
 
