4159fc663400e8fe83013e3f3c5719f29c847c04
werener <lenyazhuk@yandex.ru>
2025-09-03 12:52:56 +0300

    STAGE 1. Final cut

--- main.py
+++ main.py
@@ -7,92 +7,123 @@ import re
 LOGIN = os.getlogin()
 HOSTNAME = socket.gethostname()
 
-WINDOW_WIDTH = 512
-WINDOW_HEIGHT = 256
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 512
 
 TERMINAL_HIDDEN = True
 
-root = tk.Tk()
-root.configure(background='black', padx=3, pady=4)
-root.title(f'{LOGIN}@{HOSTNAME}')
-root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
+FONT = "Modern"
 
-container = tk.Frame(borderwidth=0, pady=0, background='red')
-terminal_text = ''
-terminal = tk.Label(text='',
-                    pady=0,
-                    padx=0,
-                    anchor='nw',
-                    background='black',
-                    justify='left',
-                    borderwidth=0,
-                    width=WINDOW_WIDTH,
-                    foreground='white')
+
+root = tk.Tk()
+root.configure(background="black", padx=3, pady=4)
+root.title(f"{LOGIN}@{HOSTNAME}")
+root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+
+container = tk.Frame(highlightthickness=0, borderwidth=0, pady=0,background="red")
+
+terminal = tk.Label(
+    
+    root,
+    highlightthickness=0,
+    text="",
+    pady=0,
+    padx=0,
+    anchor="nw",
+    background="black",
+    justify="left",
+    borderwidth=0,
+    width=WINDOW_WIDTH,
+    font=FONT,
+    foreground="white",
+)
 
 input_field = tk.Entry(
+    
     container,
-    #    cursor='green',
-    justify='left',
-    background='black',
-    foreground='white',
+    highlightthickness=0,
+    font=FONT,
+    justify="left",
+    background="black",
+    foreground="white",
     borderwidth=0,
-    selectbackground='white',
-    selectforeground='black',
-    insertbackground='white',
+    
+    selectbackground="white",
+    selectforeground="black",
+    
+    insertbackground="white",
     insertborderwidth=2,
     insertwidth=3,
+    
     insertofftime=0,
-    width=int(WINDOW_WIDTH * 0.75))
-prefix = tk.Label(container,
-                  borderwidth=0,
-                  width=15,
-                  padx=0,
-                  anchor='nw',
-                  background='black',
-                  justify='left',
-                  foreground='lime')
+    width=int(WINDOW_WIDTH * 0.75),
+)
+prefix = tk.Label(
+    container,
+    borderwidth=0,
+    width=17,
+    padx=0,
+    anchor="nw",
+    background="black",
+    justify="left",
+    foreground="lime",
+    font=FONT,
+)
 
-prefix['text'] = f'{LOGIN}@{HOSTNAME}$'
+prefix["text"] = f"{LOGIN}@{HOSTNAME}$"
 
 
 def handleExecuteButton(event=None):
 
     def add_to_terminal():
         inp = input_field.get()
-        if terminal['text'] == '':
-            terminal['text'] += f'{LOGIN}@{HOSTNAME}$  {inp}'
+        if terminal["text"] == "":
+            terminal["text"] += f"{LOGIN}@{HOSTNAME}$  {inp}"
         else:
-            terminal['text'] += f'\n{LOGIN}@{HOSTNAME}$  {inp}'
+            terminal["text"] += f"\n{LOGIN}@{HOSTNAME}$  {inp}"
 
     def handle_args():
-        # try:
-            inp = input_field.get().split()
+        inp = input_field.get().replace('\'', '"')
+        args = []
+        
+        try: 
+            command = inp.split()[0]
+            comma_args = re.findall("[\'\"].{0,}[\'\"]", inp)
             
-            command = inp[0]
+            # in case command is made of several words, i.e. 'good cd' should be a command {good cd}
+            if comma_args and command in comma_args[0]:
+                command = comma_args[0]
+            elif comma_args:
+                args.append(comma_args[0].removeprefix('"').removesuffix('"'))   
+                
+            for arg in comma_args[1:]:
+                    args.append(arg.removeprefix('"').removesuffix('"'))
+                    inp = inp.replace(arg, "")
+            args += inp.split()[1:]
 
-            def map_input(inp):
-                parameters, args = [], []
-                for arg in inp:
-                    if re.fullmatch(r"[\'\"].{0,}[\'\"]", arg):
-                        parameters.append(arg)
-                    else:
-                        args.append(arg)
-                return parameters, args
             
-            parameters, args = map_input(inp[1:])
-
+            # Command implementation
             match command:
-                case 'exit':
+                case "exit":
                     root.destroy()
-                case 'ls':
-                    terminal['text'] += '\nls  args: ' + ' '.join(args) + '  parameters: ' + ' '.join(parameters)
-                case 'cd':
-                    terminal['text'] += '\ncd  args:' + ' '.join(args) + '  parameters' + ' '.join(parameters)
+                case "ls":
+                    terminal["text"] += (
+                        "\nls\targs: "
+                        + " ".join(args)
+                    )
+                case "cd":
+                    terminal["text"] += (
+                        "\ncd\targs:"
+                        + " ".join(args)
+                    )
                 case _:
-                    terminal['text'] += f'\ncommand not found: {command}'
-
-        # except:
-        #     print('no input given')
+                    terminal["text"] += f"\ncommand not found: {command}"
+        except Exception as e:
+            print('empty input')
+        
+        
+                
+    
 
     add_to_terminal()
     handle_args()
@@ -101,38 +132,53 @@ def handleExecuteButton(event=None):
         TERMINAL_HIDDEN = False
         container.pack_forget()
 
-        terminal.pack(side='top', )
+        terminal.pack(
+            side="top",
+        )
 
-        container.pack(side='top', )
+        container.pack(
+            side="top",
+        )
 
         prefix.pack(in_=container, side="left")
         input_field.pack(
             in_=container,
-            side='left',
+            side="left",
         )
 
         input_field.focus()
-    input_field.delete(0, 'end')
+    input_field.delete(0, "end")
     input_field.focus()
 
 
-root.bind('<Return>', handleExecuteButton)
+root.bind("<Return>", handleExecuteButton)
 
-execute_button = tk.Button(command=handleExecuteButton,
-                           borderwidth=0,
-                           background="#1C1C1C",
-                           foreground="#616365",
-                           text='Execute')
-execute_button.pack(side='top', anchor=tk.NE)
+execute_button = tk.Button(
+    
+    command=handleExecuteButton,
+    borderwidth=0,
+    highlightthickness=2,
+    highlightbackground="#2B2828",
+    
+    activebackground="#3B3B3B",
+    activeforeground="#000000",
+    
+    background="#1C1C1C",
+    foreground="#616365",
+    text="Execute",
+)
+execute_button.pack(side="top", anchor=tk.NE)
 
 # terminal.pack(side='top', )
 
-container.pack(side='top', )
+container.pack(
+    side="top",
+)
 
 prefix.pack(in_=container, side="left")
 input_field.pack(
     in_=container,
-    side='left',
+    side="left",
 )
 
 input_field.focus()
